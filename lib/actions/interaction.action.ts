48911a953a5fae7f302aa60bdd76e760847649d2
JafirRehman <mjafirrehman@gmail.com>
2024-10-07 22:01:56 +0500

    fixing issues related to view

--- lib/actions/interaction.action.ts
+++ lib/actions/interaction.action.ts
@@ -22,14 +22,13 @@ export async function viewQuestion(params: ViewQuestionParams) {
         question: questionId,
       });
 
-      if (existingInteraction) return console.log("User has already viewed.");
-
-      // Create interaction
-      await Interaction.create({
-        user: userId,
-        action: "view",
-        question: questionId,
-      });
+      if (!existingInteraction) {
+        await Interaction.create({
+          user: userId,
+          action: "view",
+          question: questionId,
+        });
+      }
     }
     revalidatePath(path);
   } catch (error) {
